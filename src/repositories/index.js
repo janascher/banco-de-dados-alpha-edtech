7edaa2ce0e5fdda58cfd725cacdc0aa06e1f6946
Janaína Scher <janainascher@protonmail.com>
2022-09-21 14:38:10 -0300

    chore: add files

---
+++ src/repositories/index.js
@@ -0,0 +1,187 @@
+const dotenv = require("dotenv");
+const { Pool } = require("pg");
+const test = require("./test");
+/* const { 
+    categoryInsert, 
+    categorySelectDesc,
+    categorySelectAsc, 
+    categoryUpdate, 
+    categoryDelete 
+} = require("./category");
+const { 
+    productInsert, 
+    productSelectDesc,
+    productSelectAsc, 
+    productUpdate, 
+    productDelete 
+} = require("./products");
+const {
+    userInsert,
+    userSelectDesc,
+    userSelectAsc,
+    userUpdate,
+    userDelete,
+} = require("./users") */
+
+dotenv.config();
+
+const pool = new Pool({
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+pool.connect((err) => {
+    if (err) {
+        return console.error("Error acquiring client", err.stack);
+    }
+});
+
+// #### COLUNA CATEGORIA ####
+// --- Testando a inserção e deleção ao mesmo tempo --
+/* async function teste() {
+    const idCat = await categoryInsert(pool, ["Saia666"]);
+    console.log(idCat);
+
+    categoryDelete(pool, [idCat]);
+}
+
+teste(); */
+
+// --- Testando a inserção  --
+/* async function testInsert() {
+    categoryInsert(pool, ["Saias"]);
+}
+
+testInsert(); */
+
+// --- Testando a deleção por seu ID --
+/* async function testDelete() {
+    categoryDelete(pool, ["ee7480c8-088d-4529-9cb4-88b78a8d7f4a"]);
+}
+
+testDelete(); */
+
+// --- Testando a atualização dos dados por seu ID --
+/* async function testUpdate() {
+    categoryUpdate(pool, ["Saias", "0e3d8323-1fa5-4dff-b3f5-20f026c96a72"]);
+}
+
+testUpdate();  */
+
+// --- Testando a seleção de dados DESC --
+/* async function testSelectDesc() {
+    categorySelectDesc(pool, ["Saias"]);
+}
+
+testSelectDesc(); */
+
+// --- Testando a seleção de dados ASC --
+/* async function SelectAsc() {
+    categorySelectAsc(pool, ["Saias"]);
+}
+
+SelectAsc(); */
+
+// #### TESTE PRODUTO ####
+// --- Testando a inserção  --
+/* async function testInsert() {
+    productInsert(pool, [
+        "a1cf7b52-d680-4284-ba1a-c5d4538fd34f",
+        "cc206fd5-4063-46ed-b8a6-50802f990eab",
+        "Camisa aveludada",
+        "242",
+        "Amaro",
+        "Lorem"
+    ]);
+}
+
+testInsert(); */
+
+// --- Testando a deleção por seu ID --
+/* async function testDelete() {
+    productDelete(pool, ["d75d7978-2122-4e79-9371-acd84175ebe7"]);
+}
+
+testDelete(); */
+
+// --- Testando a atualização dos dados por seu ID --
+/* async function testUpdate() {
+    productUpdate(pool, [
+        "Camisa aveludada", 
+        "252",
+        "Amaro",
+        "Lorem ipsum dolor sit amet",
+        "e70099f3-ac47-45b6-a986-adddbf3dfbff"
+    ]);
+}
+
+testUpdate();  */
+
+// --- Testando a seleção de dados DESC --
+/* async function testSelectDesc() {
+    productSelectDesc(pool, ["Lança Perfume", "5e964c4c-2239-4237-9ba7-8dbadf7de426"]);
+}
+
+testSelectDesc(); */
+
+// --- Testando a seleção de dados ASC --
+/* async function testSelectAsc() {
+    productSelectAsc(pool, ["Lança Perfume", "5e964c4c-2239-4237-9ba7-8dbadf7de426"]);
+}
+
+testSelectAsc();  */
+
+// #### TESTE CLIENTE ####
+// --- Testando a inserção  --
+/* async function testInsert() {
+    userInsert(pool, [
+        "Cliente1",
+        "14445678952",
+        "11350585173",
+        "admin2@email.com",
+        "666333"
+    ]);
+}
+
+testInsert(); */
+
+// --- Testando a deleção por seu ID --
+/* async function testDelete() {
+    userDelete(pool, ["ea47467e-2fb0-456f-b97c-d1246f1df7e7"]);
+}
+
+testDelete(); */
+
+// --- Testando a atualização dos dados por seu ID --
+/* async function testUpdate() {
+    userUpdate(pool, [
+        "Admin2", 
+        "12345678952",
+        "11350585173",
+        "admin2@email.com",
+        "777888",
+        "72d1bd1c-852e-4edf-9a97-1f4f8ea1f6d1"
+    ]);
+}
+
+testUpdate();  */
+
+// --- Testando a seleção de dados DESC --
+/* async function testSelectDesc() {
+    userSelectDesc(pool, ["Cliente1"]);
+}
+
+testSelectDesc(); */
+
+// --- Testando a seleção de dados ASC --
+/* async function testSelectAsc() {
+    userSelectAsc(pool, ["Cliente1"]);
+}
+
+testSelectAsc();  */
